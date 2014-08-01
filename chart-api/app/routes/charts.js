871d896fb93ad95a0609e693d78b3c5ec77fd02a
Daniel Nichter <daniel@percona.com>
2014-08-01 12:52:56 -0600

    Rename graphs to data in routes. Simplify the sample data.

--- chart-api/app/routes/charts.js
+++ chart-api/app/routes/charts.js
@@ -1,6 +1,17 @@
+var data = [
+	{"date":'2014-07-01', 'data': [31, 263, 159, 22, 270]},
+	{"date":'2014-07-02', 'data': [14, 260, 158, 12, 276]},
+	{"date":'2014-07-03', 'data': [24, 265, 164, 19, 269]},
+	{"date":'2014-07-04', 'data': [23, 269, 168, 19, 276]},
+	{"date":'2014-07-05', 'data': [25, 272, 168, 17, 279]},
+	{"date":'2014-07-06', 'data': [ 5, 259, 160,  3, 283]},
+	{"date":'2014-07-07', 'data': [ 5, 257, 159,  4, 288]}
+];
+
 exports.findAll = function (req, res) {
 	res.send([{id:1,name:'mysql'}, {id:2,name:'cpu'}, {id:3,name:'disk-sda01'}]);
 };
+
 exports.findById = function (req, res) {
 	var str = '';
 	switch (req.params.id) {
@@ -8,7 +19,7 @@ exports.findById = function (req, res) {
 			str = '{"id":"' + req.params.id + '", "name": "mysql", "description": "MySQL Load"}';
 			break;
 		case '2':
-			str = '{"id":"' + req.params.id + '", "name": "cpu", "description": "CPU system"}';
+			str = '{"id":"' + req.params.id + '", "name": "cpu", "description": "CPU Usage"}';
 			break;
 		case '3':
 			str = '{"id":"' + req.params.id + '", "name": "disk-sda01", "description": "Disk Writes on /dev/sda01"}';
@@ -18,59 +29,18 @@ exports.findById = function (req, res) {
 	}
 	res.send(JSON.parse(str));
 };
-exports.getGraphsById = function (req, res) {
-	var reqDate = new Date();
-	var dd = reqDate.getDate();
-	var mm = reqDate.getMonth() + 1;
-	var yyyy = reqDate.getFullYear();
-	if (dd < 10) {
-		dd = '0' + dd;
-	}
-	if (mm < 10) {
-		mm = '0' + mm;
-	}
-	var str = '[{"date":"' + yyyy + '-' + mm + '-01' + '","s1":31,"s2":263,"s3":159,"s4":22,"s5":270},{"date":"' + yyyy + '-' + mm + '-02' + '","s1":14,"s2":260,"s3":158,"s4":12,"s5":276},{"date":"' + yyyy + '-' + mm + '-03' + '","s1":24,"s2":265,"s3":164,"s4":19,"s5":269},{"date":"' + yyyy + '-' + mm + '-04' + '","s1":23,"s2":269,"s3":168,"s4":19,"s5":276},{"date":"' + yyyy + '-' + mm + '-05' + '","s1":25,"s2":272,"s3":168,"s4":17,"s5":279},{"date":"' + yyyy + '-' + mm + '-06' + '","s1":5,"s2":259,"s3":160,"s4":3,"s5":283},{"date":"' + yyyy + '-' + mm + '-07' + '","s1":5,"s2":257,"s3":159,"s4":4,"s5":288}]';
-	res.send(JSON.parse(str));
-};
-exports.getGraphsForDate = function (req, res) {
-	var str = '';
-	var reqDate = new Date(req.params.date + ' 01:00:00');
-	var dd = reqDate.getDate();
-	var mm = reqDate.getMonth() + 1;
-	var yyyy = reqDate.getFullYear();
-	if (dd < 10) {
-		dd = '0' + dd;
-	}
-	if (mm < 10) {
-		mm = '0' + mm;
-	}
-	var newDate = yyyy + '-' + mm + '-' + dd;
-	switch (dd) {
-		case '01':
-			str = '{"date":"' + newDate + '","s1":31,"s2":263,"s3":159,"s4":22,"s5":270}';
-			break;
-		case '02':
-			str = '{"date":"' + newDate + '","s1":14,"s2":260,"s3":158,"s4":12,"s5":276}';
-			break;
-		case '03':
-			str = '{"date":"' + newDate + '","s1":24,"s2":265,"s3":164,"s4":19,"s5":269}';
-			break;
-		case '04':
-			str = '{"date":"' + newDate + '","s1":23,"s2":269,"s3":168,"s4":19,"s5":276}';
-			break;
-		case '05':
-			str = '{"date":"' + newDate + '","s1":25,"s2":272,"s3":168,"s4":17,"s5":279}';
-			break;
-		case '06':
-			str = '{"date":"' + newDate + '","s1":5,"s2":259,"s3":160,"s4":3,"s5":283}';
-			break;
-		case '07':
-			str = '{"date":"' + newDate + '","s1":5,"s2":257,"s3":159,"s4":4,"s5":288}';
-			break;
-		default:
-			str = '{"date":"' + newDate + '","s1":0,"s2":0,"s3":0,"s4":0,"s5":0}';
-	}
-	res.send(JSON.parse(str));
+
+exports.getChartData = function (req, res) {
+	res.send(data);
 };
 
-
+exports.getChartDataForDate = function (req, res) {
+	var reqDate = req.params.date // e.g. 2014-07-01
+	for (var i = 0, len = data.length; i < len; i++) {
+		if (data[i].date == reqDate) {
+			res.send(data[i]);
+			return;
+		}
+	}
+	res.send(null,404);
+};
--- chart-api/app/server.js
+++ chart-api/app/server.js
@@ -5,8 +5,8 @@ var app = express();
  
 app.get('/charts', charts.findAll);
 app.get('/charts/:id', charts.findById);
-app.get('/charts/:id/graphs', charts.getGraphsById);
-app.get('/charts/:id/graphs/:date', charts.getGraphsForDate);
+app.get('/charts/:id/data', charts.getChartData);
+app.get('/charts/:id/data/:date', charts.getChartDataForDate);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
